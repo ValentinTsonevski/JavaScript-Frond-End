366dd49f02ccd03df77d9169ddb888b7fcdab1fa
Valentin Tsonevski <tsonevski99@gmail.com>
2023-08-11 18:03:20 +0300

    Add files via upload

---
+++ JS Syntax Fundamentals - Lab/8. Circle Area.js
@@ -0,0 +1,11 @@
+function calculateCircleArea(input){
+
+    let inpytType = typeof(input);
+
+    if(inpytType === `number`){
+        console.log((Math.pow(input,2) * Math.PI).toFixed(2));
+    }else{
+      console.log(`We can not calculate the circle area, because we receive a ${inpytType}.`);
+    }
+
+}
