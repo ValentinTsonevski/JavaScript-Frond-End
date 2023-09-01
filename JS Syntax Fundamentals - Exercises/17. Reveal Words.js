981d1edf7bf83c5640b582b9f567b54b79112fd4
Valentin Tsonevski <tsonevski99@gmail.com>
2023-09-01 19:33:50 +0300

    Add files via upload

---
+++ JS Syntax Fundamentals - Exercises/17. Reveal Words.js
@@ -0,0 +1,12 @@
+function replaceWords(words, text) {
+    const wordArr = words.split(", ");
+  
+    let replacedText = text;
+  
+    for (const word of wordArr) {
+      let temp = "*".repeat(word.length);
+      replacedText = replacedText.replace(temp, word);
+    }
+  
+    console.log(replacedText);
+  }
