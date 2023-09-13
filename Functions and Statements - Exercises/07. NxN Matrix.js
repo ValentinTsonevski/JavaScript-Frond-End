5ea523d38c3ce589cf298e2ce50e83c8ecfd279d
Valentin Tsonevski <tsonevski99@gmail.com>
2023-09-13 15:17:59 +0300

    Add files via upload

---
+++ Functions and Statements - Exercises/07. NxN Matrix.js
@@ -0,0 +1,19 @@
+function printNxNNumber(number) {
+
+    let output = "";
+
+    for (let index = 0; index < number; index++) {
+        output += number + " ";
+
+    }
+
+    
+    for (let index = 0; index < number; index++) {
+        console.log(output)
+    }
+
+ 
+}
+
+
+printNxNNumber(3)
