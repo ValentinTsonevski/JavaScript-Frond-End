2b03383c580c478c693fe7a2c1492c5a7fe45b74
Valentin Tsonevski <tsonevski99@gmail.com>
2023-09-15 17:49:33 +0300

    Add files via upload

---
+++ Objects and Classes - Lab/01.Person info.js
@@ -0,0 +1,10 @@
+
+function personInfo(firstName, lastName, age) {
+
+    const person = {};
+
+    person.firstName = firstName;
+    person.lastName = lastName;
+    person.age = age;
+    return person;
+}
