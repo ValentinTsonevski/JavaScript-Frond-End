7d7c7e4c4a6c1fa07aa5ae543101be68bb3c3db9
Valentin Tsonevski <tsonevski99@gmail.com>
2023-09-15 17:50:54 +0300

    Add files via upload

---
+++ Objects and Classes - Lab/05. Convert to JSON.js
@@ -0,0 +1,12 @@
+function createPerson(name, lastName, hairColor) {
+    let person = {};
+
+    person.name = name;
+    person.lastName = lastName;
+    person.hairColor = hairColor;
+
+    console.log(JSON.stringify(person))
+}
+
+
+createPerson('George', 'Jones', 'Brown')
