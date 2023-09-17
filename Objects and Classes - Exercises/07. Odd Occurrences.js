bccc26a08906341b59866616ea525e22ac25b7e5
Valentin Tsonevski <tsonevski99@gmail.com>
2023-09-17 14:05:36 +0300

    Add files via upload

---
+++ Objects and Classes - Exercises/07. Odd Occurrences.js
@@ -0,0 +1,18 @@
+function findWordCountOdd(text){
+
+    let textArr = text.split(" ")
+
+    for (let index = 1; index <= textArr.length; index++) {
+     let current = textArr[index];
+
+     if(current === textArr[index -1]){
+
+     }
+        
+    }
+
+}
+
+
+
+findWordCountOdd('Java C# Php PHP Java PhP 3 C# 3 1 5 C#');
