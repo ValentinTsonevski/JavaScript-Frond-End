aa51ea114b17e885179593edb9172d9f46407c69
Valentin Tsonevski <tsonevski99@gmail.com>
2023-09-25 18:37:23 +0300

    Add files via upload

---
+++ DOM and Events - Lab/04. List-Of-Items/app.js
@@ -0,0 +1,8 @@
+function addItem() {
+    let newItem = document.getElementById("newItemText").value;
+
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(newItem));
+
+    document.getElementById("items").appendChild(li);
+}
